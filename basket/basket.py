f7808dc8cd0daa71ff6766553d0d640f509b25d4
Gediminas <gedoksnis@gmail.com>
2022-12-04 07:24:55 +0200

    tets ad to cart

--- basket/basket.py
+++ basket/basket.py
@@ -11,5 +11,16 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':12345}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product):
+        """
+        Naudotojų krepšelio seanso duomenų įtraukimas ir atnaujinimas
+        """
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': int(product.price)}
+
+        self.session.modified = True
