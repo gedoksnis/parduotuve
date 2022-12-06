84f1a704d414cde7ee2f841e115d7319d38a9925
Gediminas <gedoksnis@gmail.com>
2022-12-07 01:21:58 +0200

    update account

--- account/forms.py
+++ account/forms.py
@@ -61,3 +61,25 @@ class RegistrationForm(forms.ModelForm):
             {'class': 'form-control', 'placeholder': 'Repeat Password'})
 
 
+class UserEditForm(forms.ModelForm):
+
+    email = forms.EmailField(
+        label='Paskyros email (negali būti keičiama)', max_length=200, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email', 'readonly': 'readonly'}))
+
+    user_name = forms.CharField(
+        label='Vartotojo vardas', min_length=4, max_length=50, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-firstname', 'readonly': 'readonly'}))
+
+    first_name = forms.CharField(
+        label='Vardas', min_length=4, max_length=50, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Firstname', 'id': 'form-lastname'}))
+
+    class Meta:
+        model = UserBase
+        fields = ('email', 'user_name', 'first_name',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_name'].required = True
+        self.fields['email'].required = True
--- account/views.py
+++ account/views.py
@@ -71,7 +71,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 # from orders.views import user_orders
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .tokens import account_activation_token
 
@@ -84,18 +84,18 @@ from .tokens import account_activation_token
 #                   {'section': 'profile', 'orders': orders})
 
 
-# @login_required
-# def edit_details(request):
-#     if request.method == 'POST':
-#         user_form = UserEditForm(instance=request.user, data=request.POST)
+@login_required
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
 
-#         if user_form.is_valid():
-#             user_form.save()
-#     else:
-#         user_form = UserEditForm(instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
 
-#     return render(request,
-#                   'account/user/edit_details.html', {'user_form': user_form})
+    return render(request,
+                  'account/user/edit_details.html', {'user_form': user_form})
 
 
 # @login_required
